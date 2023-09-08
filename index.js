6f500a9ee5c9fd9c92bb5b39d7d79b77ec5a6f98
loop <88555889+giu08@users.noreply.github.com>
2023-09-08 02:29:54 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+import { Client, GatewayIntentBits } from 'discord.js'
+
+import { OpenAI } from "openai"
+
+//I wanna die :)
+//Me too :D
+
+const openai = new OpenAI({ apiKey: process.env['OPENAI_TOKEN'] })
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+})
+
+const TOKEN = process.env['TOKEN']
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+});
+
+client.on('messageCreate', async (message) => {
+  let id = client.user.id
+  let username = client.user.username
+  let content = message.content
+
+  async function askPeppa(question, message, model = "text-davinci-001") {
+    let finalQuestion = "sei peppa pig comunista, razzista, molto complottista e abbastanza sgarbata: " + question
+    const completion = await openai.completions.create({
+      model: model,
+      prompt: finalQuestion,
+      max_tokens: 104,
+      temperature: 0,
+    })
+
+    message.reply(completion.choices[0].text)
+    console.log(completion)
+  }
+
+  if (message.author.bot) {
+    return
+  }
+
+  if (message.mentions.has(client.user.id)) {
+    content = content.replace('<@${id}>', username)
+
+    askPeppa(content, message)
+  }
+});
+
+client.login(TOKEN)
